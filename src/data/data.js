3e5b0b822b6ac2cfdcfe3bc0047b8e04ca89e815
LabanMugutu <labanmugutu@gmail.com>
2025-08-21 19:12:37 +0300

    Finalizes Repo

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+const posts = [
+  {
+    id: 1,
+    title: "Components 101",
+    date: "December 15, 2020",
+    preview: "Setting up the building blocks of your site",
+  },
+  {
+    id: 2,
+    title: "Props and State",
+    date: "December 20, 2020",
+    preview: "Understanding data flow in React",
+  },
+  {
+    id: 3,
+    title: "Hooks in Action",
+    date: "December 25, 2020",
+    preview: "Making your components more powerful",
+  },
+];
+
+export default posts;
